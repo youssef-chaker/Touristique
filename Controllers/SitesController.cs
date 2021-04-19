bcaa4db67e7eb28b5240b5e7327863e91ae6a811
youssef-chaker <kikoulolz1youssef@gmail.com>
2021-04-19 20:46:43 +0000

    Site

--- Controllers/SitesController.cs
+++ Controllers/SitesController.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TouristiqueDbContext.models;
+using TouristiqueMvc.Models;
 
 namespace TouristiqueMvc.Controllers
 {
@@ -14,17 +19,88 @@ namespace TouristiqueMvc.Controllers
         }
 
         // GET
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string ville = null,string pays=null)
         {
-            var sites = await _db.Sites.ToListAsync();
-            return View(sites);
+            var sites = _db.Sites.AsQueryable();
+            if (pays != null) sites = sites.Where(h => h.Location.Pays == pays);
+            if (ville != null) sites = sites.Where(h => h.Location.Ville == ville);
+            var svm = new SiteViewModel();
+            svm.Sites = await sites.Include(h => h.Notes).Include(h => h.Location).ToListAsync();
+            svm.Ville = ville;
+            svm.Pays = pays;
+            return View(svm);
         }
 
         [HttpGet("[controller]/{id}")]
         public async Task<IActionResult> Site(int id)
         {
-            var site = await _db.Sites.FindAsync(id);
-            return View("Site", site);
+            Site site;
+            try
+            { 
+                site = await _db.Sites.Include(h => h.Comments).ThenInclude(c => c.User).Include(h => h.Location).Include(h => h.Notes).Where(h => h.Id == id)
+                    .FirstAsync();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return NotFound();
+            }
+            if (site == null) return NotFound();
+            return View("site",new SiteViewModel(){Site =  site});
+        }
+        
+        
+        [HttpPost]
+        public async Task<IActionResult> PostComment(int siteId,string comment)
+        {
+            var c = new SiteComment() {Comment = comment};
+            c.Site = await _db.Sites.FindAsync(siteId);
+            c.User = await _db.Users.FindAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            await _db.SiteComments.AddAsync(c);
+            await _db.SaveChangesAsync();
+            return Redirect($"{siteId}");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostRating(int siteId, int rating)
+        {
+            var r = new NoteSite()
+            {
+                Etoile = rating
+            };
+            r.Site = await _db.Sites.FindAsync(siteId);
+            r.User = await _db.Users.FindAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            
+            await _db.NoteSites.AddAsync(r);
+            
+            await _db.SaveChangesAsync();
+            return Redirect($"{siteId}");
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Post(Site site)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var location = await _db.Locations.Where(l =>
+                l.Adresse == site.Location.Adresse && l.Pays == site.Location.Pays &&
+                l.Ville == site.Location.Ville).FirstOrDefaultAsync();
+            if (location == null)
+            {
+                var newLocation = await _db.Locations.AddAsync(site.Location);
+                site.Location = newLocation.Entity;
+            }
+            else site.Location = location;
+
+            var result = await _db.Sites.AddAsync(site);
+            await _db.SaveChangesAsync();
+
+            return Redirect($"{result.Entity.Id}");
         }
     }
 }
---
+++ Models/SiteViewModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using TouristiqueDbContext.models;
+
+namespace TouristiqueMvc.Models
+{
+    public class SiteViewModel
+    {
+        public List<Site> Sites { get; set; }
+        public string Ville { get; set; }
+        public string Pays { get; set; }
+        public Site Site { get; set; }
+    }
+}
